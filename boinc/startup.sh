06c4935c09ef6436b959a69f07de6122de269067
Sumin Byeon <suminb@gmail.com>
2022-01-23 01:19:10 +0900

    Minor improvements

--- boinc/startup.sh
+++ boinc/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 /etc/init.d/boinc-client start
-echo "Wait for a few seconds..." && sleep 5
+echo "Wait for a few seconds..." && sleep 15
 boinccmd --join_acct_mgr http://bam.boincstats.com $USERNAME $PASSWORD
 boinccmd --set_network_mode always
 boinccmd --set_run_mode always
 boinccmd --set_gpu_mode auto
+sleep infinity
